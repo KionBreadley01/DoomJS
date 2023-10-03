5b74bd220f87926e300df93f29184a23ca888417
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-10-03 14:42:53 -0600

    01 Asigcronismo Javascript

---
+++ asincrono.js
@@ -0,0 +1,21 @@
+const teams = ["Liverpol","Dortmun","Chelsie"];
+
+setTimeout(() => console.log('Tarea asigcrona'));
+
+console.log('sigue ejecutando');
+console.log(teams);
+
+function getTeams{
+    return teams;
+}
+function asincGeTeams(){
+    setTimeout(() => teams , 2000);
+
+}
+console.log(getTeams());
+console.log('final de codigo');
+
+console.log(DsincGeTeans());
+console.log(DsincGeTeans());
+console.log('Termina el llamado de funcion ');
+
