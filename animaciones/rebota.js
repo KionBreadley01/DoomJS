7c160704caf7bf2faeac22cfb91cb8e7b37f4e70
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-10-25 12:45:09 -0600

    06 manu de la pagina

--- animaciones/rebota.js
+++ animaciones/rebota.js
@@ -1,51 +1,48 @@
 const lienzo = document.querySelector('#lienzo');
-const ctx= lienzo.getContext('2b');
+const ctx = lienzo.getContext('2d');
 
 let idX = 10;
 let idY = 10;
 
-let directionX=1 ;
-let directionY=1 ;
+let directionX = 1;
+let directionY = 1;
+
 let color = 0;
 
-function pelota (x, y){   
-    ctx.fillStyle = `(${color},50% , 50% )`;
+function pelota(x, y){
+    ctx.fillStyle = `hsl(${ color }, 50%, 50%)`;
     color ++;
     ctx.beginPath();
-    ctx.arc( x, y, 20, 0, Math.PI * 2);
-    ctx.fillStyle();
-    ctx.stroke();
+    ctx.arc(x, y, 30, 0, Math.PI * 2);
+    ctx.fill();
+    //ctx.stroke();
 }
-function cuadro(x, y){
-    ctx.fillStyle = `(${color},50% , 50% )`;
-    color++;
-    ctx.fillStyle(x, y, 30, 30)
-}
-
-setInterval(() => {
-    // ctx.clearReact (0, 0, 600, 400);
-    pelota (idX, idY);
-    if(directionX ===1 && directionY ==1){
-    idX += 10;
-    idY += 10;
-    }else if(directionX ===2  && directiony ===1){
-    idX -= 10;
-    idY += 10;
-    }else if(directionX===1  && directiony ===2){
-    idX += 10;
-    idY -= 10;
-    }else{
-    idX -= 10;
-    idY -= 10;
+    function cuadro( x, y){
+        ctx.fillStyle = `hsl(${ color }, 50%, 50%)`;
+        color++;
+        ctx.fillRect(x, y, 30, 30);
     }
 
-    if(idX > 600)  directionX = 2;
-    if(idX > 10)   directionX = 1;
-    if(idX > 400)  directionY = 2;
-    if(idX > 400)  directionY = 1;
+
+
+setInterval(() => {
+    //ctx.clearRect(0, 0, 600, 400);
+    cuadro(idX, idY);
+    if(directionX === 1 && directionY === 1){
+        idX +=10;
+        idY +=10;
+    }else if(directionX ===2 && directionY ===1){
+        idX -=10;
+        idY +=10;
+    }else if(directionX === 1 && directionY ===2){
+        idX +=10;
+        idY -=10;
+    }else{
+        idX -=10;
+        idY -=10;
+    }
+    if(idX > 650) directionX =2;
+    if(idX < 10) directionX =1;
+    if(idY > 400) directionY =2;
+    if(idY < 10) directionY =1;
 }, 10);
-
-
-
-    
-    
